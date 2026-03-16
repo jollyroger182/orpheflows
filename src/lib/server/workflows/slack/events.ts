88e84bc2deb6f21b11c6a9b1408e3065b8b30740
jollyroger182 <hello@jollyy.dev>
2026-03-16 21:58:47 +0800

    add info to workflow app home

--- src/lib/server/workflows/slack/events.ts
+++ src/lib/server/workflows/slack/events.ts
@@ -1,5 +1,7 @@
+import { EXTERNAL_URL } from '$env/static/private'
 import { Workflows } from '$lib/server/services'
 import { slack } from '$lib/server/slack'
+import type { ActionsBlockElement, ContextBlockElement } from '@slack/web-api'
 
 export async function handleWorkflowEvent(
 	payload: Slack.EventCallback,
@@ -10,18 +12,50 @@ export async function handleWorkflowEvent(
 
 	if (event.type === 'app_home_opened') {
 		if (event.tab !== 'home') return
+
+		const pfpElements: ContextBlockElement[] = workflow.author.photo_url
+			? [{ type: 'image', image_url: workflow.author.photo_url, alt_text: 'Profile picture' }]
+			: []
+
+		const editElements: ActionsBlockElement[] =
+			workflow.authorId === event.user
+				? [
+						{
+							type: 'button',
+							text: { type: 'plain_text', text: 'Edit workflow' },
+							url: `${EXTERNAL_URL}/workflows/${workflow.id}/edit`
+						}
+					]
+				: []
+
 		await slack.views.publish({
 			user_id: event.user,
 			view: {
 				type: 'home',
 				blocks: [
 					{
+						type: 'header',
+						text: { type: 'plain_text', text: workflow.name }
+					},
+					{
+						type: 'context',
+						elements: [
+							...pfpElements,
+							{ type: 'mrkdwn', text: `<@${workflow.author.id}>` },
+							{
+								type: 'mrkdwn',
+								text: `Created at <!date^${Math.round(workflow.createdAt.getTime() / 1000)}^{date}, {time}|${workflow.createdAt.toISOString()}>`
+							}
+						]
+					},
+					{
 						type: 'section',
-						text: { type: 'mrkdwn', text: `*${workflow.name}*` }
+						text: { type: 'mrkdwn', text: workflow.description }
 					},
 					{
 						type: 'actions',
 						elements: [
+							...editElements,
 							{
 								type: 'button',
 								text: { type: 'plain_text', text: 'Run workflow' },
