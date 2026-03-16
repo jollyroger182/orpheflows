7d9d36c80f4c00dab320f86c9b5d48b49e017d51
jollyroger182 <hello@jollyy.dev>
2026-03-16 22:24:38 +0800

    design home page a bit better

--- src/routes/(container)/+page.svelte
+++ src/routes/(container)/+page.svelte
@@ -10,20 +10,38 @@
 
 <div class="mb-4 flex items-center gap-20">
 	<h1 class="text-3xl font-semibold">Your workflows</h1>
-	<a href={resolve('/workflows/new')} class="btn btn-success">Create</a>
+	{#if user}
+		<a href={resolve('/workflows/new')} class="btn btn-success">Create</a>
+	{/if}
 </div>
+
 {#if user}
 	{#if data.workflows.length}
-		<p class="mb-4">you have {data.workflows.length} workflows</p>
+		<p class="mb-4">
+			You have {data.workflows.length} workflow{data.workflows.length > 1 ? 's' : ''}.
+		</p>
 
-		<ul>
+		<ul class="mb-4 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 			{#each data.workflows as workflow (workflow.id)}
-				<li><a href={resolve(`/workflows/${workflow.id}`)}>{workflow.name}</a></li>
+				<li>
+					<a href={resolve(`/workflows/${workflow.id}`)}>
+						<div
+							class="cursor-pointer rounded-xl border border-gray-500 p-8 transition-shadow hover:shadow-lg"
+						>
+							<h2 class="mb-4 text-xl font-semibold">{workflow.name}</h2>
+							<p>
+								Created at <time datetime={workflow.createdAt.toISOString()}
+									>{workflow.createdAt.toLocaleString()}</time
+								>
+							</p>
+						</div>
+					</a>
+				</li>
 			{/each}
 		</ul>
 	{:else}
-		you have no workflows
+		<p class="mb-4">You have no workflows.</p>
 	{/if}
 {:else}
-	you are not logged in
+	<p class="mb-4">You are not logged in. Log in to see your workflows.</p>
 {/if}
