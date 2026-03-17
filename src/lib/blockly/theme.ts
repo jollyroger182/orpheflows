20953dbfec1cdbb922803e979078d7ae48b43fe2
jollyroger182 <hello@jollyy.dev>
2026-03-17 23:53:28 +0800

    add blockly dark theme

--- src/lib/blockly/theme.ts
+++ src/lib/blockly/theme.ts
@@ -19,4 +19,25 @@ const theme = Blockly.Theme.defineTheme('orpheflows', {
 	},
 	startHats: true
 })
+
+const darkTheme = Blockly.Theme.defineTheme('orpheflows-dark', {
+	name: 'orpheflows-dark',
+	base: theme,
+	// taken from @blockly/theme-dark
+	componentStyles: {
+		workspaceBackgroundColour: '#1e1e1e',
+		toolboxBackgroundColour: '#333',
+		toolboxForegroundColour: '#fff',
+		flyoutBackgroundColour: '#252526',
+		flyoutForegroundColour: '#ccc',
+		flyoutOpacity: 1,
+		scrollbarColour: '#797979',
+		insertionMarkerColour: '#fff',
+		insertionMarkerOpacity: 0.3,
+		scrollbarOpacity: 0.4,
+		cursorColour: '#d0d0d0'
+	}
+})
+
 export default theme
+export { theme as light, darkTheme as dark }
