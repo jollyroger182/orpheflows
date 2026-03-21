7191249be56af7525412a67f63cd3161e52a2094
jollyroger182 <hello@jollyy.dev>
2026-03-22 01:12:06 +0800

    add api docs

---
+++ src/routes/docs/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import swagger from '$lib/assets/swagger.yaml'
+	import 'swagger-ui-dist/swagger-ui.css'
+	import { SwaggerUIBundle } from 'swagger-ui-dist'
+
+	let swaggerDiv: HTMLDivElement
+
+	onMount(() => {
+		console.log(swagger)
+		SwaggerUIBundle({
+			domNode: swaggerDiv,
+			spec: swagger
+		})
+	})
+</script>
+
+<div bind:this={swaggerDiv}></div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import tailwindcss from '@tailwindcss/vite'
+import ViteYaml from '@modyfi/vite-plugin-yaml'
 import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	server: {
 		allowedHosts: true
 	},
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), ViteYaml()]
 })
