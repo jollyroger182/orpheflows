b44b2ef2ad2acc32c669102e5670f34dbc332e4f
jollyroger182 <hello@jollyy.dev>
2026-04-21 22:06:59 +0800

    add api docs for variables endpoints

--- src/routes/api/workflows/[id]/variables/+server.ts
+++ src/routes/api/workflows/[id]/variables/+server.ts
@@ -11,5 +11,12 @@ export async function GET({ locals, params, request }) {
 	const workflow = await Variables.getAllByWorkflow(id)
 	if (workflow?.authorId !== userId) return error(403, 'Forbidden')
 
-	return json(workflow.variables.map((v) => ({ id: v.id, name: v.name, value: v.value })))
+	return json(
+		workflow.variables.map((v) => ({
+			id: v.id,
+			workflowId: v.workflowId,
+			name: v.name,
+			value: v.value
+		}))
+	)
 }
