f5662fca238d93fd8129eaa3ff0e0309d04894cc
jollyroger182 <hello@jollyy.dev>
2026-06-28 22:14:26 -0400

    fix array stuff

--- src/lib/server/workflows/execution/steps/lists.ts
+++ src/lib/server/workflows/execution/steps/lists.ts
@@ -27,11 +27,10 @@ export default {
 
 		const repeat = parseInt(num)
 		if (isNaN(repeat) || repeat < 0) throw new Error('Invalid repeat count')
-		const json = JSON.stringify(item)
 		return (
 			'[' +
 			Array.from({ length: repeat })
-				.map(() => json)
+				.map(() => item)
 				.join(',') +
 			']'
 		)
