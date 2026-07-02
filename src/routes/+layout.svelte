ad23080e7358ba7515157836f9e0becd1707b18a
Ingo Wolf <me@ingo.au>
2026-07-02 23:08:49 +1000

    change header styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 
 <ModeWatcher />
 
-<header class="flex items-center gap-2 bg-blue-100 px-4 py-2 dark:bg-blue-950">
+<header class="flex items-center gap-2 border-b bg-card px-4 py-2">
 	<a href={resolve('/')}><span class="text-2xl">Orpheflows</span></a>
 
 	<span class="flex-1"></span>
